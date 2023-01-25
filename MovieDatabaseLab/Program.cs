3af94136f5f0137cd9251470a278a8d9e9bc1867
jgarr27 <117048949+jgarr27@users.noreply.github.com>
2023-01-25 08:49:39 -0500

    initial

---
+++ MovieDatabaseLab/Program.cs
@@ -0,0 +1,39 @@
+﻿using MovieDatabaseLab;
+
+List<movie> Film = new List<movie>()
+{
+    //Drama, Sports, Action, Thriller
+    new movie("Dances With Wolves", "Drama"),
+    new movie("Bull Durham", "Sports"),
+    new movie("Message In a Bottle", "Drama"),
+    new movie("Waterworld", "Action"),
+    new movie("The Postman", "Drama"),
+    new movie("Mr Brooks", "Thriller"),
+    new movie("Tin Cup", "Sports"),
+    new movie("The Highway Men", "Thriller"),
+    new movie("Draft Day", "Sports"),
+    new movie("The Bodyguard", "Drama"),
+    new movie("Field of Dreams", "Drama")
+};
+
+Console.WriteLine("Welcome to the Movie List Application! ");
+Console.WriteLine();
+
+bool RunProgram = true;
+while (RunProgram)
+{
+    Console.WriteLine("There are 11 movies in this list. ");
+    Console.WriteLine("What category are you interested in? ");
+
+    string choice = Console.ReadLine().ToLower().Trim();
+
+    static string ByGenre(List<string, string> movie)
+    {
+        string choice = movie.First(movie => movie.Contains(choice));
+        {
+            if(choice == )
+        }
+    }
+
+
+
---
+++ MovieDatabaseLab/movie.cs
@@ -0,0 +1,26 @@
+﻿using MovieDatabaseLab;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDatabaseLab
+{
+    public class movie
+    {
+        //properties
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        //constructor
+        public movie(string _title, string _genre)
+        {
+            Title = _title;
+            Genre = _genre;
+        }
+        //method
+     
+
+    }
+}
+
